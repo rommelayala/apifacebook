80e958c92cbc257453759a0344a463ce613e5cc6
Capitole-Consulting <capitole-consulting@capitole.com>
2019-01-02 17:54:06 +0100

    primera subida con funciones basicas, una api, greeting con un saludo en html http://localhost:8080/greeting?name=Rommel y el muestreo de los feed y los friends del face pero no se muestran los friends

--- src/main/java/com/example/springbootherokurommel/controller/DemoAppController.java
+++ src/main/java/com/example/springbootherokurommel/controller/DemoAppController.java
@@ -1,22 +1,65 @@
 package com.example.springbootherokurommel.controller;
 
+import org.springframework.social.connect.ConnectionRepository;
+import org.springframework.social.facebook.api.Facebook;
+import org.springframework.social.facebook.api.PagedList;
+import org.springframework.social.facebook.api.Post;
+import org.springframework.social.facebook.api.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 public class DemoAppController {
 
-    @RequestMapping("/")
-    @ResponseBody
-    String home() {
-        return "Hello World!";
+    private Facebook facebook;
+    private ConnectionRepository connectionRepository;
+
+    public DemoAppController(Facebook facebook, ConnectionRepository connectionRepository) {
+        this.facebook = facebook;
+        this.connectionRepository = connectionRepository;
     }
 
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
 
     @RequestMapping("/api")
     @ResponseBody
-    String apiDemo() {
-        return "Hello api!!";
+    public String apiDemo() {
+        return "Hola desde la api!!";
     }
+
+    @RequestMapping(value = "feed", method = RequestMethod.GET)
+    public String feed(Model model) {
+
+        if(connectionRepository.findPrimaryConnection(Facebook.class) == null) {
+            return "redirect:/connect/facebook";
+        }
+
+        User userProfile = facebook.userOperations().getUserProfile();
+        model.addAttribute("userProfile", userProfile);
+        PagedList<Post> userFeed = facebook.feedOperations().getFeed();
+        model.addAttribute("userFeed", userFeed);
+        return "feed";
+    }
+    @RequestMapping(value = "friends", method = RequestMethod.GET)
+    public String friends(Model model) {
+
+        if(connectionRepository.findPrimaryConnection(Facebook.class) == null) {
+            return "redirect:/connect/facebook";
+        }
+
+        User userProfile = facebook.userOperations().getUserProfile();
+        model.addAttribute("userProfile", userProfile);
+        List<User> friends = facebook.friendOperations().getFriendProfiles();
+        model.addAttribute("friends", friends);
+
+        return "friends";
+    }
+
 }
